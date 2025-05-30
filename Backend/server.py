a30a36533dd224a39dd59be10096f3f4f5b86928
Casta-mere <castamerego@gmail.com>
2025-05-30 15:43:40 +0800

    Login/Logout use account & password Backend finish

--- Backend/server.py
+++ Backend/server.py
@@ -3,11 +3,54 @@ import os
 import sys
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
+from .functions.login import LoginManager
+from pydantic import BaseModel
 import uvicorn
 import logging
-import queue
+
+logger = logging.getLogger("uvicorn")
+
+origins = [
+    "http://localhost:8555",  
+    "http://127.0.0.1:8555", 
+]
+
+loginmanager = LoginManager()
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_credentials=True,
+    allow_methods=["*"],   
+    allow_headers=["*"],   
+)
+
+class LoginRequest(BaseModel):
+    account: str
+    password: str
+
+@app.post("/api/login")
+async def login(req: LoginRequest):
+    logger.info("Login attempt with account: %s, password: %s", req.account, req.password)
+    result = loginmanager.login(req.account, req.password)
+    return result
+
+@app.get("/api/login")
+async def loggedIn():
+    if loginmanager._is_loggedIn():
+        return {"loggedIn": True}
+    return {"loggedIn": False}
+
+@app.get("/api/user")
+async def userInfo():
+    return loginmanager.get_user_info()
+
+@app.post("/api/logout")
+async def logout():
+    logger.info(f"Logout for {loginmanager.account}")
+    loginmanager.logout()
 
 def get_static_dir():
     if hasattr(sys, "_MEIPASS"):
@@ -16,20 +59,8 @@ def get_static_dir():
 
 app.mount("/", StaticFiles(directory=get_static_dir(), html=True), name="static")
 
-log_queue = queue.Queue()
-
-class LogHandler(logging.Handler):
-    def emit(self, record):
-        log_queue.put(self.format(record))
-
 def run_backend(app):
-    logger = logging.getLogger("uvicorn")
-    logger.setLevel(logging.INFO)
-    handler = LogHandler()
-    handler.setFormatter(logging.Formatter('%(message)s'))
-    logger.addHandler(handler)
-
     uvicorn.run(app, host="127.0.0.1", port=8765, log_config=None)
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8765)
+    run_backend(app)
