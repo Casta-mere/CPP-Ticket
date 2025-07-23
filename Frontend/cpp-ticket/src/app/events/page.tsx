64d94926ecbfe3cf6c4af510e29dc90557b411a8
Casta-mere <castamerego@gmail.com>
2025-07-23 11:26:07 +0800

    Add Event Updater

--- Frontend/cpp-ticket/src/app/events/page.tsx
+++ Frontend/cpp-ticket/src/app/events/page.tsx
@@ -1,11 +1,12 @@
 "use client";
 
-import { EventCards } from "@/app/components";
+import { EventCards, EventUpdater } from "@/app/components";
 import { Flex } from "@radix-ui/themes";
 
 export default function Page() {
   return (
     <Flex direction="column" gap="5">
+      <EventUpdater />
       <EventCards />
     </Flex>
   );
