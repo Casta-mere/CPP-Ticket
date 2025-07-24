6b807c138ff74dab416f8bd07c356632f2b0aafd
Casta-mere <castamerego@gmail.com>
2025-07-24 17:02:28 +0800

    feat: Add select buyer

--- Frontend/cpp-ticket/src/app/page.tsx
+++ Frontend/cpp-ticket/src/app/page.tsx
@@ -1,3 +1,31 @@
+"use client";
+import { useUser, SelectBuyers, useSelectedBuyer } from "@/app/components";
+import { Callout, Flex, Link, Strong } from "@radix-ui/themes";
+import { GoInfo } from "react-icons/go";
+
 export default function Home() {
-  return <></>;
+  const { isLoggedIn } = useUser();
+  const { isSelected } = useSelectedBuyer();
+
+  const Tips = () => (
+    <Callout.Root variant="soft" color="blue">
+      <Callout.Icon>
+        <GoInfo />
+      </Callout.Icon>
+      <Callout.Text>
+        请提前添加好购票人，可以点击
+        <Link href="https://cp.allcpp.cn/ticket/prePurchaser" target="_blank">
+          <Strong>这里</Strong>
+        </Link>
+        添加
+      </Callout.Text>
+    </Callout.Root>
+  );
+
+  return (
+    <Flex gap="3" direction="column">
+      {!isSelected && <Tips />}
+      {isLoggedIn && <SelectBuyers />}
+    </Flex>
+  );
 }
